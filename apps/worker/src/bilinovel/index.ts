5395bdb50933ec256048d37f4708d92485ded8bb
yjl9903 <yjl9903@onekuma.cn>
2025-11-24 04:46:25 +0800

    feat(worker): add error handler

--- apps/worker/src/bilinovel/index.ts
+++ apps/worker/src/bilinovel/index.ts
@@ -17,46 +17,85 @@ app.get('/', async (c: Context) => {
 app.get('/novel/:nid', async (c: Context) => {
   const nid = c.req.param('nid');
 
-  const content = await launchBrowser(c, async (page) => {
-    await page.goto(`https://www.bilinovel.com/novel/${nid}.html`);
-    return await page.content();
-  });
+  try {
+    const content = await launchBrowser(c, async (page) => {
+      const resp = await page.goto(`https://www.bilinovel.com/novel/${nid}.html`);
+      if (!resp) throw new Error('no response');
+      if (resp.ok()) {
+        return await resp.text();
+      } else {
+        throw new Error(`${resp.status()} ${resp.statusText()} ${resp.url()}`);
+      }
+    });
 
-  return c.json({
-    ok: true,
-    provider: Provider.bilinovel,
-    html: content
-  });
+    return c.json({
+      ok: true,
+      provider: Provider.bilinovel,
+      html: content
+    });
+  } catch (error) {
+    return c.json({
+      ok: false,
+      provider: Provider.bilinovel,
+      error: (error as any).message
+    });
+  }
 });
 
 app.get('/novel/:nid/vol/:vid', async (c: Context) => {
   const nid = c.req.param('nid');
   const vid = c.req.param('vid');
 
-  const content = await launchBrowser(c, async (page) => {
-    await page.goto(`https://www.bilinovel.com/novel/${nid}/vol_${vid}.html`);
-    return await page.content();
-  });
+  try {
+    const content = await launchBrowser(c, async (page) => {
+      const resp = await page.goto(`https://www.bilinovel.com/novel/${nid}/vol_${vid}.html`);
+      if (!resp) throw new Error('no response');
+      if (resp.ok()) {
+        return await resp.text();
+      } else {
+        throw new Error(`${resp.status()} ${resp.statusText()} ${resp.url()}`);
+      }
+    });
 
-  return c.json({
-    ok: true,
-    provider: Provider.bilinovel,
-    html: content
-  });
+    return c.json({
+      ok: true,
+      provider: Provider.bilinovel,
+      html: content
+    });
+  } catch (error) {
+    return c.json({
+      ok: false,
+      provider: Provider.bilinovel,
+      error: (error as any).message
+    });
+  }
 });
 
 app.get('/novel/:nid/content/:cid', async (c: Context) => {
   const nid = c.req.param('nid');
   const cid = c.req.param('cid');
 
-  const content = await launchBrowser(c, async (page) => {
-    await page.goto(`https://www.bilinovel.com/novel/${nid}/${cid}.html`);
-    return await page.content();
-  });
+  try {
+    const content = await launchBrowser(c, async (page) => {
+      const resp = await page.goto(`https://www.bilinovel.com/novel/${nid}/${cid}.html`);
+      if (!resp) throw new Error('no response');
+      if (resp.ok()) {
+        return await resp.text();
+      } else {
+        throw new Error(`${resp.status()} ${resp.statusText()} ${resp.url()}`);
+      }
+    });
 
-  return c.json({
-    ok: true,
-    provider: Provider.bilinovel,
-    html: content
-  });
+    return c.json({
+      ok: true,
+      provider: Provider.bilinovel,
+      html: content
+    });
+  } catch (error) {
+    return c.json({
+      ok: false,
+      provider: Provider.bilinovel,
+      error: (error as any).message
+    });
+  }
 });
