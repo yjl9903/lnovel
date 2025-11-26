cd28196f834b629b3911cd8fd18b4bec806b3cf2
yjl9903 <yjl9903@onekuma.cn>
2025-11-26 23:36:13 +0800

    fix(server): bundle playwright

--- packages/bilinovel/src/fetch.ts
+++ packages/bilinovel/src/fetch.ts
@@ -2,13 +2,34 @@ import type { BrowserContext } from 'playwright';
 
 import type { BilinovelFetchOptions } from './types';
 
+export interface NovelPageResult {
+  name: string;
+  labels: string[];
+  description: string;
+  cover: string | null;
+  volumes: Array<{
+    vid: number;
+    title: string;
+    cover: string;
+    volume: string;
+  }>;
+}
+
+export interface NovelVolumePageResult {
+  name: string;
+  labels: string[];
+  description: string;
+  cover: string | null;
+  chapters: Array<{ cid: number; title: string }>;
+}
+
 export async function fetchNovelPage(
   context: BrowserContext,
-  id: number,
+  nid: number,
   options?: BilinovelFetchOptions
-) {
+): Promise<NovelPageResult | undefined> {
   const page = await context.newPage();
-  const novelURL = new URL(`/novel/${id}.html`, options?.baseURL || 'https://www.linovelib.com/');
+  const novelURL = new URL(`/novel/${nid}.html`, options?.baseURL || 'https://www.linovelib.com/');
 
   await page.goto(novelURL.toString());
 
@@ -32,7 +53,7 @@ export async function fetchNovelPage(
       const volume = await locator.locator('h4').first().textContent();
 
       const vidMatch = href?.match(/vol_(\d+)\.html/);
-      const vid = vidMatch ? +vidMatch[1] : '';
+      const vid = vidMatch ? +vidMatch[1] : 0;
 
       const imgMatch = img?.match(/url\(['"]?(.*?)['"]?\)/);
       const cover = imgMatch ? imgMatch[1] : '';
@@ -51,6 +72,56 @@ export async function fetchNovelPage(
     labels,
     description,
     cover,
-    volumes: vols.filter((v) => v.vid && v.title && v.cover && v.volume)
+    volumes: vols
+      .filter((v) => v.vid && v.title && v.cover && v.volume)
+      .sort((lhs, rhs) => lhs.vid - rhs.vid) as any
+  };
+}
+
+export async function fetchNovelVolumePage(
+  context: BrowserContext,
+  nid: number,
+  vid: number,
+  options?: BilinovelFetchOptions
+): Promise<NovelVolumePageResult | undefined> {
+  const page = await context.newPage();
+  const novelURL = new URL(
+    `/novel/${nid}/vol_${vid}.html`,
+    options?.baseURL || 'https://www.linovelib.com/'
+  );
+
+  await page.goto(novelURL.toString());
+
+  const name = await page.locator('.book-info > .book-name').first().textContent();
+
+  if (!name) return undefined;
+
+  const labels = await page.locator('.book-info > .book-label a').allTextContents();
+  const description = await page
+    .locator('.book-info > .book-dec > p:not(.backupname)')
+    .first()
+    .innerHTML();
+
+  const cover = await page.locator('.book-img > img').first().getAttribute('src');
+
+  const chapters = await Promise.all(
+    (await page.locator('.book-new-chapter > .tit > a').all()).map(async (locator) => {
+      const title = await locator.textContent();
+      const href = await locator.getAttribute('href');
+      const cidMatch = href?.match(/\/(\d+)\.html$/);
+      const cid = cidMatch ? +cidMatch[1] : 0;
+      return {
+        cid,
+        title
+      };
+    })
+  );
+
+  return {
+    name,
+    labels,
+    description,
+    cover,
+    chapters: chapters.filter((c) => c.cid && c.title) as any
   };
 }
--- scripts/browser.ts
+++ scripts/browser.ts
@@ -4,7 +4,7 @@ import {
   runBrowserContext
 } from '../apps/server/src/browser';
 
-import { fetchNovelPage } from '../packages/bilinovel/src/index';
+import { fetchNovelPage, fetchNovelVolumePage } from '../packages/bilinovel/src/index';
 
 // const broswer = connectBrowserOverCDP(
 //   'ws://127.0.0.1:9222/devtools/browser/2daf55e4-ffe4-4c36-b71f-c8f7c0f5784a'
@@ -13,6 +13,11 @@ const browser = launchBrowser();
 
 runBrowserContext<void>(browser, async (ctx) => {
   console.log('chrome has been connected...');
-  const resp = await fetchNovelPage(ctx, 4649);
-  console.log(resp);
+  const nid = 4649;
+  const novel = await fetchNovelPage(ctx, nid);
+  console.log(novel);
+  for (const volume of novel?.volumes ?? []) {
+    const info = await fetchNovelVolumePage(ctx, nid, volume.vid);
+    console.log(info);
+  }
 });
