22bd148f09c11fa4e6680a74e03a3d414aca154f
yjl9903 <yjl9903@onekuma.cn>
2025-12-09 23:37:47 +0800

    chore: rename novel related

--- packages/bilinovel/src/index.ts
+++ packages/bilinovel/src/index.ts
@@ -1,3 +1,3 @@
-export * from './fetch';
+export * from './novel';
 
 export * from './types';
---
+++ packages/bilinovel/src/wenku.ts
@@ -0,0 +1,15 @@
+import type { BrowserContext } from 'playwright';
+
+import type { BilinovelFetchOptions } from './types';
+
+export interface BilinovelFetchWenkuFilter {
+  // TODO
+}
+
+export function fetchWenkuPage(
+  context: BrowserContext,
+  filter: BilinovelFetchWenkuFilter,
+  options: BilinovelFetchOptions
+) {
+  // TODO
+}
