4937098d08740e126e6ff54e163403c83b4c34e2
yjl9903 <yjl9903@onekuma.cn>
2026-01-08 01:45:41 +0800

    fix(bili): top parse url

--- packages/bilinovel/src/top.ts
+++ packages/bilinovel/src/top.ts
@@ -174,7 +174,8 @@ export function parseTopFilter(input: URL): BilinovelFetchTopFilter {
   const searchParams = input.searchParams;
   const filter: BilinovelFetchTopFilter = {};
 
-  const sort = parseMappedParam(TOP_SORT, input.pathname.split('/').at(-1));
+  const inputSort = /\/top\/(.*)\/.*/.exec(input.pathname)?.[1]
+  const sort = parseMappedParam(TOP_SORT, inputSort || input.pathname.split('/').at(-1));
   if (sort !== undefined) filter.sort = sort;
 
   const page = parsePositiveInteger(searchParams.get('page'));
