e44dbddc28b4d66a2a6b2b41f0e11e11b0c79939
yjl9903 <yjl9903@onekuma.cn>
2026-01-16 02:19:40 +0800

    chore: init landing app

---
+++ apps/landing/vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'node:url';
+
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
--- packages/lnovel/src/cli.ts
+++ packages/lnovel/src/cli.ts
@@ -12,7 +12,7 @@ const cli = breadc('lnovel', {
     version: { description: '输出 lnovel 版本号' },
     help: { description: '输出 lnovel 帮助信息' }
   }
-})
+});
 
 cli.run(process.argv.slice(2)).catch((err) => {
   console.log(color.lightRed(`[Unknown Error] ${err.message}`));
